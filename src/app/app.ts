803354a61f22a75837656f348e3e7b47b1f3fc0c
Orodan <jimmykasprzak@gmail.com>
2026-05-01 14:35:29 +0200

    ✨ Chapter 1 : initial setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Navbar } from './shell/navbar/navbar';
+import { Sidebar } from './shell/sidebar/sidebar';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Sidebar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('my-flights-42');
+
+  protected updateTitle(): void {
+    this.title.set('Highly sophisticated flight app');
+
+    console.log('Title updated', this.title());
+  }
 }
---
+++ src/app/shell/navbar/navbar.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component, DOCUMENT, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Navbar {
+  private sidebarVisible = false;
+
+  private document = inject(DOCUMENT);
+
+  protected sidebarToggle() {
+    const body = this.document.getElementsByTagName('body')[0];
+
+    if (this.sidebarVisible) {
+      this.sidebarVisible = false;
+      body.classList.remove('nav-open');
+    } else {
+      body.classList.add('nav-open');
+      this.sidebarVisible = true;
+    }
+  }
+}
